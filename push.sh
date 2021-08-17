51f17c866105e8ac4abfbc41bb53cca90b67cf9d
Daniel Neil <danielneilhodder@gmail.com>
2021-08-17 18:36:19 +1000

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 SHARK_DIR="/shark"
+SHARK_CONF="${SHARK_DIR}/Shark-Config"
 
 # Used to verify if the config file.
 
@@ -11,9 +12,9 @@ fi
 /usr/bin/yamllint trading-config.yml
 
 # Pushes the shark config to the shark server.
-cp -p ${SHARK_DIR}/trading-config.yml ${SHARK_DIR}/_sync/
+cp -p ${SHARK_CONF}/trading-config.yml ${SHARK_DIR}/_sync/
 
 # Convert the yaml into nagios code
-${SHARK_DIR}/conf/_sync/convert_configuration.py > ${SHARK_DIR}/_sync/sync_role/files/conf.d/shark.cfg
+${SHARK_CONF}/conf/_sync/convert_configuration.py > ${SHARK_CONF}/_sync/sync_role/files/conf.d/shark.cfg
 
-ansible-playbook ${SHARK_DIR}/conf/_sync/site.yml -i _sync/hosts 
+ansible-playbook ${SHARK_CONF}/_sync/site.yml -i _sync/hosts 
