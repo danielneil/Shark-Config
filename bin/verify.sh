8328f09c64039eba1faab4908fd7a1d286407e67
Daniel Neil <danielneilhodder@gmail.com>
2021-11-02 14:43:43 +1100

    Update verify.sh

--- bin/verify.sh
+++ bin/verify.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/bash
 
-SHARK_DIR="/shark"
-SHARK_CONF="${SHARK_DIR}/Shark-Config"
-
-# Used to verify if the config file.
+echo "Verifying Shark's config"
 
 if [ ! `which yamllint` ]; then
-  dnf install -y yamllint.noarch &&  echo "Installing yamllint, exiting..."
+  echo "Install yamllint please, exiting..."
+  exit 1
 fi
 
-/usr/bin/yamllint ${SHARK_CONF}/trading-config.yml  
+XMLLINT=`/usr/bin/yamllint`
+
+${XMLLINT} trading-config.yml  
 
 if [ $? -ne 0  ]; then
  echo "Invalid YAML, exiting..." && exit 1
-fi
 
-# Pushes the shark config to the shark server.
-cd ${SHARK_CONF} && cp -p trading-config.yml _sync/
-
-# Convert the yaml into nagios code
-cd ${SHARK_CONF} && _sync/convert_configuration.py > _sync/sync_role/files/conf.d/shark.cfg
-
-cd ${SHARK_CONF}/_sync && ansible-playbook site.yml -i hosts 
+echo "Shark's YAML config looks OK..." && exit 0
