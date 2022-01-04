5b081085fabc9eaabd7891f95c9cbee28d63e672
Daniel Neil <danielneilhodder@gmail.com>
2022-01-05 00:36:59 +1100

    Update convert_configuration.py

--- bin/config/files/convert_configuration.py
+++ bin/config/files/convert_configuration.py
@@ -79,7 +79,7 @@ hostGroups = []
 serviceGroups = []
 services = StringBuilder();
 
-with open ("trading-config.yml", "r") as f:
+with open ("/var/www/git/trading-config.yml", "r") as f:
 
     # Load YAML data from the file
 
