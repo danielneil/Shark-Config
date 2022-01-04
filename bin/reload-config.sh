e8e4e87686680f7aa8712deffb9d07bd124ed64c
root <root@localhost.localdomain>
2022-01-04 23:05:02 +1100

    amended structure

--- bin/reload-config.sh
+++ bin/reload-config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-if ! $(which /usr/bin/ansible); then
+if ! $(which ansible-playbook > /dev/null); then
  echo "Install ansible, and run again, exiting.."
  exit 1;
 fi
 
-ansible-playbook site.yml -i hosts
+ansible-playbook ./site.yml 
