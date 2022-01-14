b89eac3f15939d70370783f3e0bcc06a4fbcbe8d
Daniel Neil <danielneilhodder@gmail.com>
2022-01-15 02:18:06 +1100

    Update backtest_moving_averages.py

--- backtests/backtest_moving_averages.py
+++ backtests/backtest_moving_averages.py
@@ -201,7 +201,7 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--ticker", help="Ticker of the stock to run the backtest against.")
     parser.add_argument("-s", "--shares", help="The number of imaginary shares to purchase.")
     parser.add_argument("-c", "--capital", help="The imaginary amount of capital available (in dollars).")
-    parser.add_argument("-p", "--peroid", help="The sma period that we will use as the basis for the cross over threshold.")
+    parser.add_argument("-p", "--period", help="The sma period that we will use as the basis for the cross over threshold.")
     parser.add_argument("-n", "--data_format", help="The provider of the historical data.")
     
     args = parser.parse_args()
@@ -218,8 +218,8 @@ if __name__ == "__main__":
         print("UNKNOWN - No capital amount specified")
         sys.exit(UNKNOWN)
 
-    if not args.peroid:
-        print("UNKNOWN - No peroid specified")
+    if not args.period:
+        print("UNKNOWN - No period specified")
         sys.exit(UNKNOWN)
 
     if not args.data_format:
