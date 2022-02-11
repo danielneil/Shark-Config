62dca499a4cb351e3e400e9692aa120edb490779
root <root@172-105-187-181.ip.linodeusercontent.com>
2022-02-11 17:16:19 +1100

    reformatted layout and add rsi2 example

--- backtests/files/backtests/_functions.py
+++ backtests/files/backtests/_functions.py
@@ -37,10 +37,10 @@ def GenerateJSONReport(strat, retAnalyzer, sharpeRatioAnalyzer, drawDownAnalyzer
 
         json_obj['backtest_summary'].append({
             'ticker': ticker,
-            'final_portfolio_value': strat.getResult(),
-            'cumulative_returns': (retAnalyzer.getCumulativeReturns()[-1] * 100),
-            'sharpe_ratio': sharpeRatio,
-            'max_drawdown': (drawDownAnalyzer.getMaxDrawDown() * 100),
+            'final_portfolio_value': "{:.2f}".format(strat.getResult()),
+            'cumulative_returns': "{:.2f}".format((retAnalyzer.getCumulativeReturns()[-1] * 100)),
+            'sharpe_ratio': "{:.2f}".format(sharpeRatio),
+            'max_drawdown': "{:.2f}".format((drawDownAnalyzer.getMaxDrawDown() * 100)),
             'longest_drawdown_duration': str(drawDownAnalyzer.getLongestDrawDownDuration()),
             'total_trades': str(tradesAnalyzer.getCount()), 
             'wins': str(tradesAnalyzer.getProfitableCount()),
@@ -61,14 +61,14 @@ def GenerateJSONReport(strat, retAnalyzer, sharpeRatioAnalyzer, drawDownAnalyzer
             json_obj['total_trades'] = []
 
             json_obj['total_trades'].append({
-                'avg_profit': profits.mean(),
-                'profits_std_dev': profits.std(),
-                'max_profit': profits.max(),
-                'min_profit': profits.min(),
-                'avg_return': (returns.mean() * 100),
-                'returns_std_dev': (returns.std() * 100),
-                'max_return': (returns.max() * 100),
-                'min_return': (returns.min() * 100)
+                'avg_profit': "{:.2f}".format(profits.mean()),
+                'profits_std_dev': "{:.2f}".format(profits.std()),
+                'max_profit': "{:.2f}".format(profits.max()),
+                'min_profit': "{:.2f}".format(profits.min()),
+                'avg_return': "{:.2f}".format((returns.mean() * 100)),
+                'returns_std_dev': "{:.2f}".format((returns.std() * 100)),
+                'max_return': "{:.2f}".format((returns.max() * 100)),
+                'min_return': "{:.2f}".format((returns.min() * 100))
                 })
 
             json.dump(json_obj, f)
@@ -85,14 +85,14 @@ def GenerateJSONReport(strat, retAnalyzer, sharpeRatioAnalyzer, drawDownAnalyzer
             json_obj['profitable_trades'] = []
 
             json_obj['profitable_trades'].append({
-                'avg_profit':  profits.mean(),
-                'profits_std_dev': profits.std(), 
-                'max_profit': profits.max(),
-                'min_profit': profits.min(),
-                'avg_return': (returns.mean() * 100),
-                'returns_std_dev': (returns.std() * 100),
-                'max_return': (returns.max() * 100),
-                'min_return': (returns.min() * 100)
+                'avg_profit':  "{:.2f}".format(profits.mean()),
+                'profits_std_dev': "{:.2f}".format(profits.std()), 
+                'max_profit': "{:.2f}".format(profits.max()),
+                'min_profit': "{:.2f}".format(profits.min()),
+                'avg_return': "{:.2f}".format((returns.mean() * 100)),
+                'returns_std_dev': "{:.2f}".format((returns.std() * 100)),
+                'max_return': "{:.2f}".format((returns.max() * 100)),
+                'min_return': "{:.2f}".format((returns.min() * 100))
                 })
 
             json.dump(json_obj, f)
@@ -109,14 +109,14 @@ def GenerateJSONReport(strat, retAnalyzer, sharpeRatioAnalyzer, drawDownAnalyzer
             json_obj['unprofitable_trades'] = []
 
             json_obj['unprofitable_trades'].append({
-                'avg_loss': losses.mean(),
-                'losses_std_dev': losses.std(),
-                'max_loss': losses.min(),
-                'min_loss': losses.max(),
-                'avg_return': (returns.mean() * 100),
-                'returns_std_dev': (returns.std() * 100),
-                'max_return': (returns.max() * 100),
-                'min_return': (returns.min() * 100)
+                'avg_loss': "{:.2f}".format(losses.mean()),
+                'losses_std_dev': "{:.2f}".format(losses.std()),
+                'max_loss': "{:.2f}".format(losses.min()),
+                'min_loss': "{:.2f}".format(losses.max()),
+                'avg_return': "{:.2f}".format((returns.mean() * 100)),
+                'returns_std_dev': "{:.2f}".format((returns.std() * 100)),
+                'max_return': "{:.2f}".format((returns.max() * 100)),
+                'min_return': "{:.2f}".format((returns.min() * 100))
                 })
 
             json.dump(json_obj, f)
